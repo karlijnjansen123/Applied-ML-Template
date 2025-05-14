be108b305ed846b5af2d07a9d978a6a14464996f
karlijnjansen123 <karlijnjjansen@gmail.com>
2025-05-14 19:14:14 +0200

    Fixed classes
    Classes are indexed to 0 instead of 1.
    Social media has a scale of 0-18
    
    Co-Authored-By: ilsetentije674 <210524662+ilsetentije674@users.noreply.github.com>

--- project_name/models/NeuralNetwork.py
+++ project_name/models/NeuralNetwork.py
@@ -4,10 +4,10 @@ from tensorflow import keras
 def build_neural_network(X,Y1,Y2,Y3,Y4):
     #Convert from pandas to tensor
     X_tensor = tf.convert_to_tensor(X.values,dtype=tf.float32)
-    Y1_tensor =tf.convert_to_tensor(Y1.values,dtype=tf.float32)
-    Y2_tensor = tf.convert_to_tensor(Y2.values, dtype=tf.float32)
-    Y3_tensor = tf.convert_to_tensor(Y3.values, dtype=tf.float32)
-    Y4_tensor = tf.convert_to_tensor(Y4.values, dtype=tf.float32)
+    Y1_tensor =tf.convert_to_tensor(Y1.values-1,dtype=tf.int32)
+    Y2_tensor = tf.convert_to_tensor(Y2.values-1, dtype=tf.int32)
+    Y3_tensor = tf.convert_to_tensor(Y3.values-1, dtype=tf.int32)
+    Y4_tensor = tf.convert_to_tensor(Y4.values-1, dtype=tf.int32)
 
     #Input Layer
     inp = tf.keras.Input(shape=(5,))
@@ -19,7 +19,7 @@ def build_neural_network(X,Y1,Y2,Y3,Y4):
     hidden4 = tf.keras.layers.Dense(16, activation='relu')(hidden3)
 
     #Output Layers
-    out1 = tf.keras.layers.Dense(9, activation ='softmax', name="Social_media")(hidden4)
+    out1 = tf.keras.layers.Dense(19, activation ='softmax', name="Social_media")(hidden4)
     out2 = tf.keras.layers.Dense(5, activation='softmax',name='think_body')(hidden4)
     out3 = tf.keras.layers.Dense(5, activation='softmax', name='feeling_low')(hidden4)
     out4 = tf.keras.layers.Dense(5, activation='softmax', name='sleep_difficulty')(hidden4)
