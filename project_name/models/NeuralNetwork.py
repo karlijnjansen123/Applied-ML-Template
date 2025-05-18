4e2e66a46c9bb76df4b3369be6ffb97261594787
karlijnjansen123 <karlijnjjansen@gmail.com>
2025-05-18 15:58:29 +0200

    Changes neural network
    Normalization of the x-features, train/test split, print statements for checking imbalanced classes

--- project_name/models/NeuralNetwork.py
+++ project_name/models/NeuralNetwork.py
@@ -1,13 +1,31 @@
 import tensorflow as tf
 from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils.class_weight import compute_class_weight
+import numpy as np
 
 def build_neural_network(X,Y1,Y2,Y3,Y4):
-    #Convert from pandas to tensor
-    X_tensor = tf.convert_to_tensor(X.values,dtype=tf.float32)
-    Y1_tensor =tf.convert_to_tensor(Y1.values,dtype=tf.int32)
-    Y2_tensor = tf.convert_to_tensor(Y2.values-1, dtype=tf.int32)
-    Y3_tensor = tf.convert_to_tensor(Y3.values-1, dtype=tf.int32)
-    Y4_tensor = tf.convert_to_tensor(Y4.values-1, dtype=tf.int32)
+    #print statements to check inbalanced classes
+    print(Y1.value_counts(normalize=True).round(3))
+    print(Y2.value_counts(normalize=True).round(3))
+    print(Y3.value_counts(normalize=True).round(3))
+    print(Y4.value_counts(normalize=True).round(3))
+
+    #splitting in test and train data
+    X_train,X_test,Y1_train,Y1_test,Y2_train,Y2_test,Y3_train,Y3_test,Y4_train,Y4_test = train_test_split(X,Y1,Y2,Y3,Y4,test_size=0.2)
+
+    #Normalisation of the x-features
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    #Convert the train data from pandas to tensor
+    X_tensor = tf.convert_to_tensor(X_train,dtype=tf.float32)
+    Y1_tensor =tf.convert_to_tensor(Y1_train.values,dtype=tf.int32)
+    Y2_tensor = tf.convert_to_tensor(Y2_train.values-1, dtype=tf.int32)
+    Y3_tensor = tf.convert_to_tensor(Y3_train.values-1, dtype=tf.int32)
+    Y4_tensor = tf.convert_to_tensor(Y4_train.values-1, dtype=tf.int32)
 
     #Input Layer
     inp = tf.keras.Input(shape=(5,))
@@ -48,5 +66,4 @@ def build_neural_network(X,Y1,Y2,Y3,Y4):
         'sleep_difficulty': Y4_tensor
         }, epochs=10, batch_size=32, validation_split=0.2
     )
-
     return model
