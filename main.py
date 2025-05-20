f240c960b53d78a8d7568f3364c586db7269f337
ilsetentije674 <ilsetentije04@gmail.com>
2025-05-20 13:44:32 +0200

    Changed Y1 (social media) from target to input
    After TA meeting, concluded to use the sum of the 9 social media questions as input instead of target output.

--- main.py
+++ main.py
@@ -10,31 +10,27 @@ print("Current working directory: ", os.getcwd())
 filepath = os.path.join(os.getcwd(), "project_name", "data", "HBSC2018.csv")
 print(filepath)
 
-# Define which columns you care about
+# the columns for input
 cols_of_interest = ["sex", "health", "timeexe", "sweets_2", "sleepdificulty", "emcsocmed1", "emcsocmed2", "emcsocmed3", "emcsocmed4", "emcsocmed5", "emcsocmed6", "emcsocmed7", "emcsocmed8", "emcsocmed9", "thinkbody", "feellow", "beenbullied"]
 
-# Columns to aggregate
+# social media columns to aggregate
 emc_cols = ["emcsocmed1", "emcsocmed2", "emcsocmed3", "emcsocmed4", "emcsocmed5", "emcsocmed6", "emcsocmed7", "emcsocmed8", "emcsocmed9"]
 
+# preprocess
 clean_data = preprocessing.preprocess_hbsc_data(filepath, cols_of_interest, emc_cols)
-
 print(clean_data.head())
 
-X = clean_data[["sex", "health", "timeexe", "sweets_2", "beenbullied"]]
-Y1 = clean_data["emcsocmed_sum"]
-Y2 = clean_data["thinkbody"]
-Y3 = clean_data["feellow"]
-Y4 = clean_data["sleepdificulty"]
-print("X shape:", X.shape)
-print("Y1 shape:", Y1.shape)
-print("Y1 NaNs:", Y1.isna().sum())
+# input and output
+X = clean_data[["sex", "health", "timeexe", "sweets_2", "beenbullied", "emcsocmed_sum"]]
+Y1 = clean_data["thinkbody"]
+Y2 = clean_data["feellow"]
+Y3 = clean_data["sleepdificulty"]
 
-# KNN Accuracy
+print("X shape:", X.shape)
+
+# run KNN model
 print(KNN_solver(X, Y1))
 print(KNN_solver(X, Y2))
 print(KNN_solver(X, Y3))
-print(KNN_solver(X, Y4))
 
 
-#if __name__ == '__main__':
- #   hello_world()
