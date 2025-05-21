a852768ecbeb1bd6e465b044eec3f5912d78a76e
ilsetentije674 <ilsetentije04@gmail.com>
2025-05-21 16:33:30 +0200

    Neural network delete tensor convert
    No tensor convert explicitly needed. Model.fit will do this internally. However, -1 is needed in model.fit to create 0-based index.

--- project_name/models/NeuralNetwork.py
+++ project_name/models/NeuralNetwork.py
@@ -7,12 +7,12 @@ import numpy as np
 import keras
 
 def build_neural_network(X, Y1, Y2, Y3,input_size):
-    #print statements to check inbalanced classes
+    #Print statements to check imbalanced classes
     print(Y1.value_counts(normalize=True).round(3))
     print(Y2.value_counts(normalize=True).round(3))
     print(Y3.value_counts(normalize=True).round(3))
 
-    #splitting in test and train data
+    #Splitting in test and train data
     X_train,X_test,Y1_train,Y1_test,Y2_train,Y2_test,Y3_train,Y3_test = train_test_split(X, Y1, Y2, Y3, test_size=.2)
 
     #Print statement for checking with and without using stratify
@@ -25,12 +25,6 @@ def build_neural_network(X, Y1, Y2, Y3,input_size):
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
-
-    #Convert the train data from pandas to tensor
-    X_tensor = tf.convert_to_tensor(X_train,dtype=tf.float32)
-    Y1_tensor = tf.convert_to_tensor(Y1_train.values-1, dtype=tf.int32)
-    Y2_tensor = tf.convert_to_tensor(Y2_train.values-1, dtype=tf.int32)
-    Y3_tensor = tf.convert_to_tensor(Y3_train.values-1, dtype=tf.int32)
 
     #Input Layer
     inp = tf.keras.Input(shape=(input_size,))
@@ -61,12 +55,13 @@ def build_neural_network(X, Y1, Y2, Y3,input_size):
             'sleep_difficulty': tf.keras.metrics.SparseCategoricalAccuracy()
         }
     )
-    model.fit(X_tensor, {
-        'think_body': Y1_tensor,
-        'feeling_low': Y2_tensor,
-        'sleep_difficulty': Y3_tensor
-        }, epochs=10, batch_size=32, validation_split=0.2
-    )
+    model.fit(X_train, {
+        'think_body': Y1_train - 1, # make it a 0-based index
+        'feeling_low': Y2_train - 1,
+        'sleep_difficulty': Y3_train - 1
+    }, epochs=10, batch_size=32, validation_split=0.2)
+
     #Evaluating the model on the test data
     #results = model.evaluate(X_test,Y1_test,Y2_test,Y3_test)
+
     return model
