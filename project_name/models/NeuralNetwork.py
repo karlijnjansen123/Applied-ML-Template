4b561d930a498eb014469bd584580b958939356f
karlijnjansen123 <karlijnjjansen@gmail.com>
2025-05-26 17:43:04 +0200

    Manually updated NeuralNetwork.py
    Manually adjusted the NeuralNetwork.py file so its the same as dev
    
    Co-Authored-By: aliesjaverheij <181573605+aliesjaverheij@users.noreply.github.com>

--- project_name/models/NeuralNetwork.py
+++ project_name/models/NeuralNetwork.py
@@ -63,5 +63,6 @@ def build_neural_network(X, Y1, Y2, Y3,input_size):
 
     #Evaluating the model on the test data
     #results = model.evaluate(X_test,Y1_test,Y2_test,Y3_test)
-
-    return model
+    #conert to tensor
+    # model = model(tf.convert_to_tensor(X_train[:1].astype(np.float32)))
+    return model, X_train ,X_test, scaler
