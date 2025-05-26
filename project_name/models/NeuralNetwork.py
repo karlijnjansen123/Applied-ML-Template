606f595a2db1ba5663be239e35b62e1e34c1fdaa
karlijnjansen123 <karlijnjjansen@gmail.com>
2025-05-26 17:41:59 +0200

    Updated NeuralNetwork.py manually
    Manually updated the NeuralNetwork.py file from the branch neural_network
    
    Co-Authored-By: aliesjaverheij <181573605+aliesjaverheij@users.noreply.github.com>

--- project_name/models/NeuralNetwork.py
+++ project_name/models/NeuralNetwork.py
@@ -5,14 +5,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils.class_weight import compute_class_weight
 import numpy as np
 import keras
+from focal_loss import  SparseCategoricalFocalLoss
 
 def build_neural_network(X, Y1, Y2, Y3,input_size):
-    #print statements to check inbalanced classes
+    #Print statements to check imbalanced classes
     print(Y1.value_counts(normalize=True).round(3))
     print(Y2.value_counts(normalize=True).round(3))
     print(Y3.value_counts(normalize=True).round(3))
 
-    #splitting in test and train data
+    #Splitting in test and train data
     X_train,X_test,Y1_train,Y1_test,Y2_train,Y2_test,Y3_train,Y3_test = train_test_split(X, Y1, Y2, Y3, test_size=.2)
 
     #Print statement for checking with and without using stratify
@@ -26,21 +27,14 @@ def build_neural_network(X, Y1, Y2, Y3,input_size):
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
-    #Convert the train data from pandas to tensor
-    X_tensor = tf.convert_to_tensor(X_train,dtype=tf.float32)
-    Y1_tensor = tf.convert_to_tensor(Y1_train.values-1, dtype=tf.int32)
-    Y2_tensor = tf.convert_to_tensor(Y2_train.values-1, dtype=tf.int32)
-    Y3_tensor = tf.convert_to_tensor(Y3_train.values-1, dtype=tf.int32)
-
     #Input Layer
     inp = tf.keras.Input(shape=(input_size,))
 
     #Hidden Layers
     hidden1 = tf.keras.layers.Dense(128, activation= 'relu')(inp)
     hidden2 = tf.keras.layers.Dense(64, activation='relu')(hidden1)
-    hidden3 = tf.keras.layers.Dense(32, activation='relu')(hidden2)
-    hidden4 = tf.keras.layers.Dense(16, activation='relu')(hidden3)
-
+    hidden3 = tf.keras.layers.Dense(64, activation='relu')(hidden2)
+    hidden4 = tf.keras.layers.Dense(64, activation='relu')(hidden3)
     #Output Layers
     out1 = tf.keras.layers.Dense(5, activation='softmax',name='think_body')(hidden4)
     out2 = tf.keras.layers.Dense(5, activation='softmax', name='feeling_low')(hidden4)
@@ -51,9 +45,9 @@ def build_neural_network(X, Y1, Y2, Y3,input_size):
     model.compile(
         optimizer='adam',
         loss={
-            'think_body': tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-            'feeling_low': tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-            'sleep_difficulty': tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+            'think_body': SparseCategoricalFocalLoss( gamma=2),
+            'feeling_low': SparseCategoricalFocalLoss(gamma =2),
+            'sleep_difficulty':SparseCategoricalFocalLoss(gamma =2)
         },
         metrics={
             'think_body': tf.keras.metrics.SparseCategoricalAccuracy(),
@@ -61,16 +55,14 @@ def build_neural_network(X, Y1, Y2, Y3,input_size):
             'sleep_difficulty': tf.keras.metrics.SparseCategoricalAccuracy()
         }
     )
-    model.fit(X_tensor, {
-        'think_body': Y1_tensor,
-        'feeling_low': Y2_tensor,
-        'sleep_difficulty': Y3_tensor
-        }, epochs=1, batch_size=32, validation_split=0.2
-    )
+    model.fit(X_train, {
+        'think_body': Y1_train - 1, # make it a 0-based index
+        'feeling_low': Y2_train - 1,
+        'sleep_difficulty': Y3_train - 1
+    }, epochs=10, batch_size=32, validation_split=0.2)
+
     #Evaluating the model on the test data
     #results = model.evaluate(X_test,Y1_test,Y2_test,Y3_test)
-    
-    #convert to tensor
-    #model = model(tf.convert_to_tensor(X_train[:1].astype(np.float32)))
-    
-    return model, X_train, X_test, scaler #X_train, X_test and scaler are remembered for SHAP
+    #conert to tensor
+    # model = model(tf.convert_to_tensor(X_train[:1].astype(np.float32)))
+    return model, X_train ,X_test, scaler
