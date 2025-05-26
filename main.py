76304801117250cf5e6ce5bfa3e1110d5a832a60
fvogelaar <francienvogelaar@gmail.com>
2025-05-26 14:31:58 +0200

    merge problem

--- main.py
+++ main.py
@@ -28,11 +28,6 @@ y = ["thinkbody", "feellow", "sleepdificulty"]
 clean_data = preprocessing.preprocess_hbsc_data(filepath, all_columns_minus_socmed, emc_cols, y)
 print(clean_data.head())
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 198185a (functionality for analyzing whole dataset inst. of subset)
 # input and output
 #X = clean_data[["sex", "health", "timeexe", "sweets_2", "beenbullied", "emcsocmed_sum"]]
 X = clean_data[["emcsocmed_sum", "fasfamcar", "fasbedroom", "fascomputers", "fasbathroom", "fasdishwash", "fasholidays", "health", "lifesat", "headache", "stomachache", "backache", "irritable", "nervous", "dizzy", "physact60", "breakfastwd", "breakfastwe", "fruits_2", "vegetables_2", "sweets_2", "softdrinks_2", "fmeal", "toothbr", "timeexe", "smokltm", "smok30d_2", "alcltm", "alc30d_2", "drunkltm", "drunk30d", "cannabisltm_2", "cannabis30d_2", "bodyweight", "bodyheight", "likeschool", "schoolpressure", "studtogether", "studhelpful", "studaccept", "teacheraccept", "teachercare", "teachertrust", "bulliedothers", "beenbullied", "cbulliedothers", "cbeenbullied", "fight12m", "injured12m", "friendhelp", "friendcounton", "friendshare", "friendtalk", "hadsex", "agesex", "contraceptcondom", "contraceptpill", "motherhome1", "fatherhome1", "stepmohome1", "stepfahome1", "fosterhome1", "elsehome1_2", "employfa", "employmo", "employnotfa", "employnotmo", "talkfather", "talkmother", "talkstepmo", "famhelp", "famsup", "famtalk", "famdec", "MBMI", "IRFAS", "IRRELFAS_LMH", "IOTF4", "oweight_who"]]
@@ -50,15 +45,10 @@ column_names = ["emcsocmed_sum", "fasfamcar", "fasbedroom", "fascomputers", "fas
 size_input = X.shape[1]
 print(size_input)
 
-
 print("X shape:", X.shape)
-<<<<<<< HEAD
-=======
 print("X NaNs", X.isna().sum().sum())
 print("Y1 shape:", Y1.shape)
 print("Y1 NaNs:", Y1.isna().sum())
->>>>>>> 198185a (functionality for analyzing whole dataset inst. of subset)
-
 print("preview of Y values", Y1.head)
 
 # run KNN model
@@ -74,47 +64,7 @@ acc, X_tr, X_te, predict_proba = KNN_solver(X, Y3)
 print("Accuracy for Sleep Difficulty:", acc)
 #KNN_shap_graphs(X_tr, X_te, predict_proba, "Sleep Difficulty", column_names=column_names)
 
-
-
 #run the neural network
-<<<<<<< HEAD
-neural_network = build_neural_network(X,Y1,Y2,Y3,size_input)
-=======
-#column_names = [i for i in clean_data.columns]
-#print(column_names)
-column_names = ["sex", "health", "timeexe", "sweets_2", "sleepdificulty"]
-X = clean_data[["sex", "health", "timeexe", "sweets_2", "sleepdificulty"]]
-Y1 = clean_data["emcsocmed_median"]
-Y2 = clean_data["thinkbody"]
-Y3 = clean_data["feellow"]
-Y4 = clean_data["sleepdificulty"]
-print("X shape:", X.shape)
-print("Y1 shape:", Y1.shape)
-print("Y1 NaNs:", Y1.isna().sum())
-
-
-acc, X_tr, X_te, predict_proba = KNN_solver(X, Y1)
-print(acc)
-shap_graphs(X_tr, X_te, predict_proba, column_names=column_names)
-
-acc, X_tr, X_te, predict_proba = KNN_solver(X, Y2)
-print(acc)
-shap_graphs(X_tr, X_te, predict_proba, column_names=column_names)
-
-acc, X_tr, X_te, predict_proba = KNN_solver(X, Y3)
-print(acc)
-shap_graphs(X_tr, X_te, predict_proba, column_names=column_names)
-
-acc, X_tr, X_te, predict_proba = KNN_solver(X, Y4)
-print(acc)
-shap_graphs(X_tr, X_te, predict_proba, column_names=column_names)
-
-
-
-#if __name__ == '__main__':
- #   hello_world()
->>>>>>> 2e5b088 (added functionality for KNN and shap as two separate funtions)
-=======
 neural_network, X_train, X_test, scaler = build_neural_network(X,Y1,Y2,Y3,size_input)
 print(type(neural_network))
 NN_shap_graphs(
@@ -122,4 +72,3 @@ NN_shap_graphs(
     X_train,
     column_names
 )
->>>>>>> 198185a (functionality for analyzing whole dataset inst. of subset)
