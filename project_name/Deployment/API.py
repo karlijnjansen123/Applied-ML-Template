75cb4f9b5cbe604ae100bc751f1ca1abf63d57ef
karlijnjansen123 <karlijnjjansen@gmail.com>
2025-05-28 10:55:13 +0200

    Model Deployment
    Loaded the model, defining the input,output format. Making predictions

--- project_name/Deployment/API.py
+++ project_name/Deployment/API.py
@@ -1,8 +1,15 @@
+"""
+Start the API from the rootpath:
+uvicorn project_name.Deployment.API:app --reload
+"""
+
 from fastapi import FastAPI
+from numpy.ma.core import argmax
 from pydantic import BaseModel
 import numpy as  np
 import keras
 from focal_loss import SparseCategoricalFocalLoss
+
 
 #Create an instance of the FastAPI, this main object will handle requests
 app = FastAPI()
@@ -30,3 +37,29 @@ class ModelInput(BaseModel):
     fastcomputers: int
     dizzy: int
     overweight: int
+#Added to get it to work?
+@app.get("/")
+async def root():
+    return {"message": "✅ The API is running. Use /predict to get predictions."}
+#Define a route (what comes after the url
+@app.post("/predict")
+#The parameter for this asynchronous function is an instance of the ModelInput
+async def make_predicition(input_data:ModelInput):
+    user_input = np.array([[input_data.irritable, input_data.nervous,
+                input_data.bodyweight,input_data.lifesat,input_data.headache,input_data.stomachache,
+                input_data.health, input_data.bodyheight,input_data.backache,input_data.studyaccept,input_data.beenbullied,
+                input_data.schoolpressure,input_data.talkfather,input_data.fastcomputers,input_data.dizzy,input_data.overweight]])
+    #Predict, returns a  list of numpy arrays, one for each output
+    prediction  = loaded_model.predict(user_input)
+
+    #Formatting the output to JSON format (from numpy to a python list), and stored in a dictionary
+    #Also encoding the class, np.argmax returns the index of the highest value
+    output = {
+        "ThinkBody": prediction[0][0].tolist(),
+        "ThinkBodyClass": int(np.argmax(prediction[0][0])),
+        "FeelingLow": prediction[1][0].tolist(),
+        "FeelingLowClass":int(np.argmax(prediction[1][0])),
+        "Sleep Difficulties" : prediction[2][0].tolist(),
+        "SleepDifficultiesClass" :int(np.argmax(prediction[2][0]))}
+    print(output)
+    return output
