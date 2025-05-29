0134d8c24e47cde6bcfc615e75925b9c62e6d972
karlijnjansen123 <karlijnjjansen@gmail.com>
2025-05-29 21:16:54 +0200

    flake8check KNN, NN,preprocessing
    small adjustment flake8 in NeuralNetwork, KNN,preprocessing

--- project_name/features/feature_correlation.py
+++ project_name/features/feature_correlation.py
@@ -2,11 +2,13 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 def feature_correlation_plot(input):
     X_df = pd.DataFrame(input)
     corr_matrix = X_df.corr()
     plt.figure(figsize=(12, 10))
-    sns.heatmap(corr_matrix, cmap='coolwarm', annot=True, fmt=".2f", square=True)
+    sns.heatmap(corr_matrix, cmap='coolwarm',
+                annot=True, fmt=".2f", square=True)
     plt.title("Feature Correlation Heatmap")
     plt.tight_layout()
     plt.show()
--- project_name/models/NeuralNetwork.py
+++ project_name/models/NeuralNetwork.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import StandardScaler
 from focal_loss import SparseCategoricalFocalLoss
 import joblib
 
+
 def test_train_split(X, Y1, Y2, Y3):
     # Print statements to check imbalanced classes
     print(Y1.value_counts(normalize=True).round(3))
@@ -23,11 +24,15 @@ def test_train_split(X, Y1, Y2, Y3):
     print(Y1_train.value_counts(normalize=True).round(3))
     print('Class distribution for the test set of Y3')
     print(Y1_test.value_counts(normalize=True).round(3))
-    return X_train, X_test, Y1_train, Y1_test, Y2_train, Y2_test, Y3_train, Y3_test
+    return (X_train, X_test,
+            Y1_train, Y1_test,
+            Y2_train, Y2_test,
+            Y3_train, Y3_test)
 
 
-def build_neural_network(X_train, X_test, Y1_train, Y1_test, Y2_train, Y2_test, Y3_train, Y3_test, size_input):
-
+def build_neural_network(X_train, X_test, Y1_train, Y1_test,
+                         Y2_train, Y2_test, Y3_train, Y3_test,
+                         size_input):
     # Normalisation of the x-features
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
@@ -69,17 +74,22 @@ def build_neural_network(X_train, X_test, Y1_train, Y1_test, Y2_train, Y2_test, 
         }
     )
     history = model.fit(X_train, {
-        'think_body': Y1_train - 1,  # make it a 0-based index
+        'think_body': Y1_train - 1,  # Make it a 0-based index
         'feeling_low': Y2_train - 1,
         'sleep_difficulty': Y3_train - 1
     }, epochs=1, batch_size=32, validation_split=0.2)
 
     # Extract the latest validation accuracies from history
-    val_accuracy_thinkbody = history.history['val_think_body_sparse_categorical_accuracy'][-1]
-    val_accuracy_feelinglow = history.history['val_feeling_low_sparse_categorical_accuracy'][-1]
-    val_accuracy_sleepdiff = history.history['val_sleep_difficulty_sparse_categorical_accuracy'][-1]
-
+    val_accuracy_thinkbody = history.history[
+        'val_think_body_sparse_categorical_accuracy'][-1]
+    val_accuracy_feelinglow = history.history[
+        'val_feeling_low_sparse_categorical_accuracy'][-1]
+    val_accuracy_sleepdiff = history.history[
+        'val_sleep_difficulty_sparse_categorical_accuracy'][-1]
     # Saving the model in the Deployment directory
     model.save('project_name/Deployment/neural_network_model.keras')
 
-    return model, X_train, X_test, scaler, val_accuracy_thinkbody, val_accuracy_feelinglow, val_accuracy_sleepdiff
+    return (model, X_train, X_test,
+            scaler, val_accuracy_thinkbody,
+            val_accuracy_feelinglow,
+            val_accuracy_sleepdiff)
